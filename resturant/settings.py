b0a12be17f0be58ae6d99ee15c69774c4659863d
Hossein Khajvandi <h.khajvandi@gmail.com>
2023-01-12 16:23:49 +0330

    Update settings.py

--- resturant/settings.py
+++ resturant/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'resturant.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : 'root@123',
     }
 }
 
